8c07af7dd71d9e120ccbb7f470530d7458ef8046
erhan <erhan.16k@gmail.com>
2024-03-19 15:55:23 +0100

    reorganised

--- public/src/CategoriesList.jsx
+++
@@ -1,9 +0,0 @@
-export const CategoriesList = ({ categories, setActiveCategory, activeCategory }) => (
-    <div className="categories">
-        {categories.map(category => (
-          <div key={category} onClick={() => setActiveCategory(category)} className={activeCategory === category ? 'categories-item categories-item-selected' : 'categories-item'}>
-            {category}
-          </div>
-        ))}
-    </div>
-  );
--- public/src/ProductsList.jsx
+++
@@ -1,9 +0,0 @@
-import {Product} from './Product'
-
-export const ProductsList = ({ products }) => (
-    <ul className="products">
-      {products.map(product => (
-        <Product key={product.id} product={product} />
-      ))}
-    </ul>
-  );
